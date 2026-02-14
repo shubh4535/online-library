c2b69272e5eeb743706bea24bc2c7bf9ecb01a21
Shubham Singh <shubh4535@gmail.com>
2026-02-15 00:04:36 +0530

    Create NotFound page to handle invalid routes with user-friendly message

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="not-found">
+      <h2>404 - Page Not Found</h2>
+      <p>Oops! The page you are looking for does not exist.</p>
+      <Link to="/">Go Back to Home</Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
